867cc99b0515a512b821a2c04dac679ace144f43
Aldrin Navarro <aldrinnavarro16@gmail.com>
2019-03-03 00:12:37 +0800

    Install black as pre-commit hook

--- ogpt/apps/govproject/models.py
+++ ogpt/apps/govproject/models.py
@@ -3,73 +3,59 @@ from django.contrib.auth.models import User
 
 
 class GovernmentProject(models.Model):
-    title = models.CharField(
-        max_length=255,
-        help_text='The title of the project'
-    )
+    title = models.CharField(max_length=255, help_text="The title of the project")
     description = models.TextField(
         blank=True,
-        help_text='Describes the purpose of the project, '
-                  'the main beneficiaries and may include '
-                  'a short text of the proposal'
+        help_text="Describes the purpose of the project, "
+        "the main beneficiaries and may include "
+        "a short text of the proposal",
     )
     total_project_cost = models.FloatField(
-        null=True,
-        blank=True,
-        help_text='The Total Project Cost on record'
+        null=True, blank=True, help_text="The Total Project Cost on record"
     )
     implementing_agency = models.ForeignKey(
-        'Agency',
-        related_name='projects',
+        "Agency",
+        related_name="projects",
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        help_text='Agency in charge'
+        help_text="Agency in charge",
     )
     implementation_period = models.DurationField(
-        null=True,
-        blank=True,
-        help_text='Duration of the project'
+        null=True, blank=True, help_text="Duration of the project"
     )
     # When implementation period is unknown
     # provide a way to indicate start and end
     implementation_from = models.DateTimeField(
-        null=True,
-        blank=True,
-        help_text='Start datetime of implementation'
+        null=True, blank=True, help_text="Start datetime of implementation"
     )
     implementation_to = models.DateTimeField(
-        null=True,
-        blank=True,
-        help_text='End datetime of implementation'
+        null=True, blank=True, help_text="End datetime of implementation"
     )
     funding_source = models.ForeignKey(
-        'FundingSource',
-        related_name='projects',
+        "FundingSource",
+        related_name="projects",
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        help_text='Source of funding'
+        help_text="Source of funding",
     )
     administrative_area = models.ForeignKey(
-        'Region',
-        related_name='projects',
+        "Region",
+        related_name="projects",
         on_delete=models.PROTECT,
-        help_text='Target region of the project'
+        help_text="Target region of the project",
     )
 
     class Meta:
-        app_label = 'govproject'
-        ordering = ('pk',)
+        app_label = "govproject"
+        ordering = ("pk",)
 
 
 class ICCDate(models.Model):
-    timestamp = models.DateTimeField(
-        help_text='Datetime of ICCDate instance'
-    )
+    timestamp = models.DateTimeField(help_text="Datetime of ICCDate instance")
     description = models.CharField(
-        max_length=180,
-        help_text='Optional: Short text describing the date'
+        max_length=180, help_text="Optional: Short text describing the date"
     )
 
     class Meta:
@@ -78,34 +64,29 @@ class ICCDate(models.Model):
 
 class ICCTBDate(ICCDate):
     """ICC-Techincal Board date"""
+
     project = models.ForeignKey(
         GovernmentProject,
-        related_name='icc_tb_dates',
+        related_name="icc_tb_dates",
         on_delete=models.CASCADE,
-        help_text='Reference to project'
+        help_text="Reference to project",
     )
 
 
 class ICCCCDate(ICCDate):
     """ICC-Cabinet Committee date"""
+
     project = models.ForeignKey(
         GovernmentProject,
-        related_name='icc_cc_dates',
+        related_name="icc_cc_dates",
         on_delete=models.CASCADE,
-        help_text='Reference to project'
+        help_text="Reference to project",
     )
 
 
 class Agency(models.Model):
-    name = models.CharField(
-        max_length=255,
-        help_text='Name of the agency'
-    )
-    shortname = models.CharField(
-        max_length=180,
-        blank=True,
-        help_text='Abbreviation'
-    )
+    name = models.CharField(max_length=255, help_text="Name of the agency")
+    shortname = models.CharField(max_length=180, blank=True, help_text="Abbreviation")
 
 
 class ProjectLog(models.Model):
@@ -113,31 +94,22 @@ class ProjectLog(models.Model):
     label = models.CharField(max_length=510)
     added_by = models.ForeignKey(
         User,
-        related_name='added_logs',
+        related_name="added_logs",
         on_delete=models.SET_NULL,
         null=True,
-        blank=True
+        blank=True,
     )
 
 
 class Region(models.Model):
     name = models.CharField(max_length=255)
     lat = models.DecimalField(
-        'Latitude',
-        max_digits=18,
-        decimal_places=12,
-        blank=True,
-        null=True
+        "Latitude", max_digits=18, decimal_places=12, blank=True, null=True
     )
     lng = models.DecimalField(
-        'Longitude',
-        max_digits=18,
-        decimal_places=12,
-        blank=True,
-        null=True
+        "Longitude", max_digits=18, decimal_places=12, blank=True, null=True
     )
 
 
 class FundingSource(models.Model):
-    name = models.CharField(max_length=255)
-
+    name = models.CharField(max_length=255, help_text="Name of the funding source")
