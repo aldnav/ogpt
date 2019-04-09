1994f700f376a964f11bd411734853dc9bfea249
Aldrin Navarro <aldrinnavarro16@gmail.com>
2019-04-09 21:55:09 +0800

    UI improvements
    Change log:
    - Revamped home page
    - Can now filter by admin area
    - Display administrative areas applied on filter
    - Simpler projects table
    - Add navbar
    - Humanize money column
    - Add project status indicator

--- ogpt/apps/govproject/tables.py
+++ ogpt/apps/govproject/tables.py
@@ -1,15 +1,27 @@
 import django_tables2 as tables
+
+from django.contrib.humanize.templatetags.humanize import intcomma
+from django.utils.html import mark_safe
+
 from .models import GovernmentProject
 
 
 class GovernmentProjectTable(tables.Table):
-    title = tables.TemplateColumn('<a href="{{ record.url }}">{{ value }}</a>')
-    description = tables.Column(orderable=False)
+    title = tables.Column("Project")
+    # description = tables.Column(orderable=False)
     implementing_agency = tables.Column()
     total_project_cost = tables.Column()
     funding_source = tables.Column("Source of funding")
     implementation_time_info = tables.Column("Implementation Period")
-    administrative_area = tables.Column(empty_values=())
+    # administrative_area = tables.Column(empty_values=())
+
+    def render_title(self, record, value):
+        return mark_safe(
+            '<a href="{0.url}"><span class="status status-{0.status}" title="{0.status}"></span>&nbsp;{0.title}</a>'.format(record)
+        )
+
+    def render_total_project_cost(self, record, value):
+        return intcomma(value)
 
     def render_administrative_area(self, record, value):
         return ",".join(record.administrative_area.all().values_list("name", flat=True))
